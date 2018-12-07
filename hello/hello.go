6e43d72a33c4bfe204aa7f235361118efb1eedbf
David Jia <david.jia@grabtaxi.com>
2018-12-06 18:04:42 -0800

    Add string util lib

--- hello/hello.go
+++ hello/hello.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/david-jia-grab/myCode/stringutil"
 )
 
 func main() {
 	fmt.Println("Hello world")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
